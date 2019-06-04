d7d0dc5a877dedfcb16b737bb772eedff7c7292a
Lee Hitchcock <>
2019-06-04 18:35:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import NewComponent from './components/newComponent';
+
+class App extends Component {
+	render() {
+		return <React.Fragment>
+			<NewComponent/>
+		</React.Fragment>
+	}
+}
+
+
+export default App;
---
+++ src/components/FilterGroup/FilterGroup.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import FilterButton from '../FilterButton/FilterButton';
+
+function filterGroup(props) {
+
+	return(
+		<div key={props.filterGroupKey}>
+			<p>{props.filterGroupName}</p>
+			<p>{props.filterGroupType}</p>
+
+			{props.filterLabels.map((label, i) => {
+				return (
+					<FilterButton
+						key={i}
+						filterLabel={label}
+						clickHandler={props.clickHandler}
+					/>
+				)
+			})}
+		</div>
+	)
+}
+
+export default filterGroup;
